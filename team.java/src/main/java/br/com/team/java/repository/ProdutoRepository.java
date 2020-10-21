2db5b97ead507fd6f8b797d1c9f873162a4da54c
Alex Monteiro Ramos <amramos@brq.com>
2020-10-21 14:26:05 -0300

    correção da Repository

--- team.java/src/main/java/br/com/team/java/repository/ProdutoRepository.java
+++ team.java/src/main/java/br/com/team/java/repository/ProdutoRepository.java
@@ -1,5 +1,11 @@
 package br.com.team.java.repository;
 
-public class ProdutoRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import br.com.team.java.model.ProdutoModel;
+
+@Repository
+	public interface ProdutoRepository extends JpaRepository<ProdutoModel, Integer> {
+		
 }
