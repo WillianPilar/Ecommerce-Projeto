28d1a6c36a109d91b12bf3e3b2054002e64f3f08
Bruno Willian Fagundes <brunobwf@brq.com>
2020-10-22 11:02:53 -0300

    Inserção do CORS

--- front/src/app/adm/usuarios/usuarios.service.spec.ts
+++ front/src/app/adm/usuarios/usuarios.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { UsuariosService } from './usuarios.service';
+import { UsuariosService } from '../adm-service-folder/usuarios.service';
 
 describe('UsuariosService', () => {
   let service: UsuariosService;
