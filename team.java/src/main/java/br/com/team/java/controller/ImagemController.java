63fdfa9c447a879564880e0edee8eb786f1a180b
Alex Monteiro Ramos <amramos@brq.com>
2020-10-23 11:24:46 -0300

    Criação das classes imagem part 2

--- team.java/src/main/java/br/com/team/java/controller/ImagemController.java
+++ team.java/src/main/java/br/com/team/java/controller/ImagemController.java
@@ -1,5 +1,8 @@
 package br.com.team.java.controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class ImagemController {
 
 }
--- team.java/src/main/java/br/com/team/java/service/ImagemService.java
+++ team.java/src/main/java/br/com/team/java/service/ImagemService.java
@@ -1,5 +1,8 @@
 package br.com.team.java.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class ImagemService {
 
 }
