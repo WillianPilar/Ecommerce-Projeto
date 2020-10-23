edbb8eccadb17ca057277b4499cb28ff9867648c
Felipe dos Santos Conceicao <fconceicao@brq.com>
2020-10-23 11:54:06 -0300

    EndPoint findAll()

--- team.java/src/main/java/br/com/team/java/controller/ImagemController.java
+++ team.java/src/main/java/br/com/team/java/controller/ImagemController.java
@@ -1,8 +1,26 @@
 package br.com.team.java.controller;
 
-import org.springframework.stereotype.Controller;
+import java.util.List;
 
-@Controller
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.team.java.model.Imagem;
+import br.com.team.java.repository.ImagemRepository;
+
+@RestController
+@RequestMapping("/imagem")
 public class ImagemController {
+	
+	@Autowired
+	ImagemRepository imagemRepository;
+	
+	
+	public ResponseEntity<List<Imagem>>getAll(){
+		List<Imagem> l = this.imagemRepository.findAll();
+		return ResponseEntity.ok().body(l);
+	}
 
 }
--- team.java/src/main/java/br/com/team/java/service/ImagemService.java
+++ team.java/src/main/java/br/com/team/java/service/ImagemService.java
@@ -1,8 +1,21 @@
 package br.com.team.java.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import br.com.team.java.model.Imagem;
+import br.com.team.java.repository.ImagemRepository;
 
 @Service
 public class ImagemService {
-
+	
+	@Autowired
+	private ImagemRepository imagemRepository;
+		
+	public List<Imagem> findAll(){
+		return this.imagemRepository.findAll();
+	}
+	
 }
