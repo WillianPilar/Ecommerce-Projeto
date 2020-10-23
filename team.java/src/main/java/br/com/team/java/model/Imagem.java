130dd919391410cb47ab4f53390b7619f20db1ff
BRQ\gabrielguedes <gabrielguedes@brq.com>
2020-10-23 11:33:53 -0300

    criação da imagem modelo com as relações

--- team.java/src/main/java/br/com/team/java/model/Imagem.java
+++ team.java/src/main/java/br/com/team/java/model/Imagem.java
@@ -1,5 +1,35 @@
 package br.com.team.java.model;
 
-public class Imagem {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+@Table(name = "team_imagem")
+public class Imagem {
+	@Id
+	@GeneratedValue (strategy = GenerationType.SEQUENCE, generator = "team_imagem_seq_id")
+	@SequenceGenerator (name = "team_imagem_seq_id", sequenceName = "team_imagemo_seq_id", allocationSize = 1)
+	private int id;
+	private int url;
+	
+	@ManyToOne
+	@JoinColumn (name ="imagem_id")
+	private Produto produto;
 }
