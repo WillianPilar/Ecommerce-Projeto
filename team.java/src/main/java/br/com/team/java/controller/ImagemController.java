c8d095ddf12957e7f6f7e0fc99d4a8c9eedb4e0b
Alex Monteiro Ramos <amramos@brq.com>
2020-10-23 12:07:40 -0300

    Imagem save e delete

--- team.java/src/main/java/br/com/team/java/controller/ImagemController.java
+++ team.java/src/main/java/br/com/team/java/controller/ImagemController.java
@@ -1,8 +1,33 @@
 package br.com.team.java.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import br.com.team.java.model.Imagem;
+import br.com.team.java.service.ImagemService;
+
+@RestController
+@RequestMapping (value="imagem")
 public class ImagemController {
+	
+	private ImagemService imagemService;
+	
+	
+	@DeleteMapping(value="{id}")
+	public void delete(@PathVariable("id") int id) {
+		this.imagemService.delete(id);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Imagem> save(@RequestBody Imagem imagem) {
+		Imagem img = this.imagemService.save(imagem);
+		return ResponseEntity.ok().body(img);
+	}
+	
 
 }
--- team.java/src/main/java/br/com/team/java/service/ImagemService.java
+++ team.java/src/main/java/br/com/team/java/service/ImagemService.java
@@ -1,8 +1,26 @@
 package br.com.team.java.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import br.com.team.java.model.Imagem;
+import br.com.team.java.repository.ImagemRepository;
+
+
+
 
 @Service
 public class ImagemService {
+	
+	@Autowired
+	private ImagemRepository imagemRepository;
 
+	
+	public void delete(int id) {
+		this.imagemRepository.deleteById(id);
+	}
+	
+	public Imagem save(Imagem imagem) {
+		return this.imagemRepository.save(imagem);
+	}
 }
