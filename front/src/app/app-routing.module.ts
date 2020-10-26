e565b47b8ea5a1d5364c247596a4a6bca0bde7a3
Alex Monteiro Ramos <amramos@brq.com>
2020-10-26 12:11:54 -0300

    criação modulo venda

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -5,13 +5,14 @@ import { GuardsService } from './adm/guards/guards.service';
 const routes : Routes = [
   {path : 'adm', loadChildren : ()=> import('./adm/adm.module').then(m=> m.AdmModule ), canActivate:[GuardsService]},
   {path : 'login', loadChildren : ()=> import('./adm/usuarios/login/login.module').then(m=> m.LoginModule )},
-  {path : 'cadastro', loadChildren : ()=> import('./adm/usuarios/cadastro/cadastro.module').then(m=> m.CadastroModule)}
+  {path : 'cadastro', loadChildren : ()=> import('./adm/usuarios/cadastro/cadastro.module').then(m=> m.CadastroModule)},
+  {path : 'venda', loadChildren : ()=> import('./venda/vendas/vendas.module').then(m=> m.VendasModule)}
 ];
 @NgModule({
   declarations: [],
   imports: [
     RouterModule.forRoot(routes),
-   
+
   ],
   exports: [RouterModule]
 })
