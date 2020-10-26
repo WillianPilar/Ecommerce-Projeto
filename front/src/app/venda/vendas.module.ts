60b412353c9e17650d49dd0db9ef4d7de48622d3
Alex Monteiro Ramos <amramos@brq.com>
2020-10-26 12:28:25 -0300

    refatoração modulo venda

--- front/src/app/venda/vendas.module.ts
+++ front/src/app/venda/vendas.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { VendasRoutingModule } from './vendas-routing.module';
+import { CarrinhoComponent } from './carrinho/carrinho.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [CarrinhoComponent],
   imports: [
     CommonModule,
     VendasRoutingModule
