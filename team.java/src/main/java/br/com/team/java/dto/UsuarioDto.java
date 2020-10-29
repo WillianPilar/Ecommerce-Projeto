719556d9374a3ae34f1b726ddb8cd3edbf6f1b16
Bruno Willian Fagundes <brunobwf@brq.com>
2020-10-29 09:24:21 -0300

    Paginacao DTO Usuario

--- team.java/src/main/java/br/com/team/java/dto/UsuarioDto.java
+++ team.java/src/main/java/br/com/team/java/dto/UsuarioDto.java
@@ -1,5 +1,6 @@
 package br.com.team.java.dto;
 
+import org.modelmapper.ModelMapper;
 
 import br.com.team.java.model.Endereco;
 import br.com.team.java.model.Usuario;
@@ -8,19 +9,25 @@ import lombok.Data;
 @Data
 public class UsuarioDto {
 
-		private int id;
-		private String nome;
-		private String email;	
-		private Endereco endereco;
+	private int id;
+	private String nome;
+	private String email;
+	private Endereco endereco;
 
-		public UsuarioDto ( Usuario usuario ) {
-			this.id = usuario.getId();
-			this.nome = usuario.getNome();
-			this.email = usuario.getEmail();
-			this.endereco = usuario.getEndereco();
-		}
-		
-		
+	public UsuarioDto(Usuario usuario) {
+		this.id = usuario.getId();
+		this.nome = usuario.getNome();
+		this.email = usuario.getEmail();
+		this.endereco = usuario.getEndereco();
+	}
+
+	public Usuario toEntity() {
+		ModelMapper modelMapper = new ModelMapper();
+		// user here is a prepopulated User instance
+		Usuario entity = modelMapper.map(this, Usuario.class);
+		return entity;
+	}
+
 //		public Usuario transformaParaObjeto(){
 //			//int senha = null;
 //		    return new Usuario(id, nome, email, endereco, null, null);
