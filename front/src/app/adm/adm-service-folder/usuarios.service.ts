697cfd23085391d04d44b34895ca924797986583
Felipe dos Santos Conceicao <fconceicao@brq.com>
2020-11-03 09:23:58 -0300

    Ajustando Apontamento

--- front/src/app/adm/adm-service-folder/usuarios.service.ts
+++ front/src/app/adm/adm-service-folder/usuarios.service.ts
@@ -2,46 +2,52 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { UsuarioPagination } from 'src/app/shared/models/usuario-pagination';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuariosService {
+
+  url = `${environment.urlApi}/usuarios`;
+  urlEndereco = `${environment.urlApi}/enderecos`;
+  urlAutenticacao = `${environment.urlApi}/autenticacao`;
+
   constructor(private httpClient:HttpClient) { }
 
   public consultarUsuarios(){
-    return this.httpClient.get("http://localhost:8081/usuarios");
+    return this.httpClient.get(this.url);
   }
 
   public consultarUsuarioId(id:Number){
-    return this.httpClient.get(`http://localhost:8081/usuarios/${id}`);
+    return this.httpClient.get(`${this.url}/${id}`);
   }
 
   public cadastrarUsuario(body){
-    return this.httpClient.post("http://localhost:8081/usuarios",body);
+    return this.httpClient.post(this.url,body);
   }
 
   public alterarUsuario(id:Number,body:any){
-    return this.httpClient.patch(`http://localhost:8081/usuarios/${id}`,body);
+    return this.httpClient.patch(`${this.url}/${id}`,body);
   }
 
   public deletarUsuario(id:number){
-    return this.httpClient.delete(`http://localhost:8081/usuarios/${id}`);
+    return this.httpClient.delete(`${this.url}/${id}`);
   }
 
   //LOGIN
   public logar(body:any){
-    return this.httpClient.post('http://localhost:8081/autenticacao',body);
+    return this.httpClient.post(this.urlAutenticacao,body);
   }
 
   //Paginação
   public paginationLike(pagina: number, linhas: number, nome: string){
-    return this.httpClient.get<UsuarioPagination>(`http://localhost:8081/usuarios/paginadorLike?pagina=${pagina}&linhas=${linhas}&nome=${nome}`);
+    return this.httpClient.get<UsuarioPagination>(`${this.url}/paginadorLike?pagina=${pagina}&linhas=${linhas}&nome=${nome}`);
   }
 
   //endereco
   public cadastrarEndereco(body){
-    return this.httpClient.post("http://localhost:8081/enderecos",body);
+    return this.httpClient.post(this.urlEndereco,body);
   }
   //Service buscar CEP
   public buscarCEP(cep){
--- front/src/app/venda/Venda-Services/venda.service.ts
+++ front/src/app/venda/Venda-Services/venda.service.ts
@@ -1,15 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VendaService {
 
+  url = `${environment.urlApi}/venda`
+
   constructor(private httpClient : HttpClient) { }
 
   public finalizarCompra(body){
-    return this.httpClient.post(`http://localhost:8081/venda`, body);
+    return this.httpClient.post(this.url, body);
   }
 
 }
